a97cd7972b944058b3b0feb411636de21a37423a
sachin5551 <79634964+sachin5551@users.noreply.github.com>
2024-07-27 16:01:48 +0530

    Add files via upload

---
+++ providers.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = ">= 5.31.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+  profile = "terraform"
+}
+
+provider "aws" {
+  alias   = "west"
+  region  = "us-west-1"
+  profile = "terraform"
+}
